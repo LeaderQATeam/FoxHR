a076feda1a74526590352f0a1dfe55f09da72072
Amina <aminahamdy345@gmail.com>
2023-01-04 14:31:56 +0200

    login

--- FoxHR/src/main/java/Pages/LoginPage.java
+++ FoxHR/src/main/java/Pages/LoginPage.java
@@ -4,5 +4,7 @@ public class LoginPage {
 
     @FindBy(xpath="//*[@placeholder=\"User Name\"]")
     private WebElement userNameTxt;
+    @FindBy(xpath="//*[@placeholder=\"Password\"]")
+    private WebElement passwordTxt;
 
 }
